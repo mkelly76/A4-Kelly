8c06754d02c55f97cd08ff17dcb7c048f9300778
mkelly76 <mkellytpc@gmail.com>
2024-03-19 16:07:22 -0400

    Create a ToDo App

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+     import '../style.css'
+     let todoItem = '';
+     let todoList = [];
+
+     function addToArray() {
+          if (todoItem == '') {
+               return;
+          }
+          //todoList.push(todoItem);
+          //todoList = todoList;
+          todoList = [...todoList, {
+               text: todoItem,
+               done: false
+          }];
+          console.log(todoList);
+          todoItem = '';
+     }
+     function removeThis(index) {
+          todoList.splice(index, 1);
+          todoList = todoList;
+     }
+</script>
+
+<h1>AgendaZen</h1>
+
+<form on:submit|preventDefault={addToArray}>
+     <input type="text" bind:value={todoItem}>
+     <button type="submit">Add</button>
+</form>
+
+<ul>
+     {#each todoList as item, index}
+          <li>
+               <input type="checkbox" bind:checked={item.done}>
+               <span class:done={item.done}>{item.text}</span>
+               <span on:click={() => removeThis(index)} 
+               class="remove" role="button" tabindex="0">&times;</span>
+          </li>
+     {/each}
+</ul>
+
+<style>
+     ul {
+          list-style: none;
+     }
+     li {
+          font-size: 1.3rem;
+     }
+     .done {
+          color: #ccc;
+          text-decoration: line-through;
+     }
+     .remove {
+          color: darkred;
+          cursor: pointer;
+     }
+</style>

a6a6344e8fdf87159cc877f2a89fae285ba3f083
mkelly76 <mkellytpc@gmail.com>
2024-03-22 21:13:11 -0400

    Added categories

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
      import '../style.css'
      let todoItem = '';
+     let urgent, important, normal, low, someday; 
      let todoList = [];
+
      $: isDone = todoList.filter(item => item.done);
+     $: somedayList = todoList.filter(item => item.someday)
+
      function addToArray() {
           if (todoItem == '') {
                return;
@@ -11,10 +15,18 @@
           //todoList = todoList;
           todoList = [...todoList, {
                text: todoItem,
-               done: false
+               done: false,
+               urgent: urgent,
+               important: important,
+               normal: normal,
+               low: low,
+               someday: someday
           }];
           console.log(todoList);
           todoItem = '';
+
+          todoList.splice(index,1);
+          todoList = todoList;
      }
      function removeThis(index) {
           todoList.splice(index, 1);
@@ -30,6 +42,21 @@
 <form on:submit|preventDefault={addToArray}>
      <input type="text" bind:value={todoItem}>
      <button type="submit">Add</button>
+     <p>Choose Category</p>
+     <input type="checkbox" name="urgent" id="urgent" class="urgent" bind:checked={urgent}>
+     <label for="urgent">Urgent</label>
+     <input type="checkbox" name="important" id="important" bind:checked={important}>
+     <label for="important">Important</label>
+     <input type="checkbox" name="normal" id="normal" bind:checked={normal}>
+     <label for="normal">Normal</label>
+     <input type="checkbox" name="low" id="low" bind:checked={low}>
+     <label for="low">Low</label>
+     <input type="checkbox" name="someday" id="someday" bind:checked={someday}>
+     <label for="someday">Someday</label>
+     <div>
+          <button type="submit">Add</button>
+     </div>
+     
 </form>
 
 <ul>
@@ -47,6 +74,7 @@
 <button on:click={clearDone}>Remove Done</button>
 {/if}
 
+
 <style>
      ul {
           list-style: none;
@@ -61,5 +89,8 @@
      .remove {
           color: darkred;
           cursor: pointer;
+     }
+     .urgent {
+          color: red;
      }
 </style>
